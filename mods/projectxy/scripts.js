eeac9bf2ea5d12d93903e054c0628b2a01450ad4
Smog S. Goon <iSmogoon2@gmail.com>
2013-05-22 19:46:08 -0500

    update scripts.js

--- mods/projectxy/scripts.js
+++ mods/projectxy/scripts.js
@@ -21,7 +21,46 @@ exports.BattleScripts = {
                 this.modData('Learnsets', 'vileplume').learnset.lunardance = ['5L55'];
                 this.modData('Learnsets', 'venomoth').learnset.defendorder = ['5L0'];
                 this.modData('Learnsets', 'dugtrio').learnset.wildcharge = ['5M'];
-        			
+                this.modData('Learnsets', 'primeape').learnset.hijumpkick = ['5L48'];
+                this.modData('Learnsets', 'primeape').learnset.bulletpunch = ['5L0'];
+                this.modData('Learnsets', 'primeape').learnset.machpunch = ['5L27'];
+                this.modData('Learnsets', 'poliwrath').learnset.closecombat = ['5L39'];
+                this.modData('Learnsets', 'poliwrath').learnset.bulletpunch = ['5L25'];
+                this.modData('Learnsets', 'poliwrath').learnset.machpunch = ['5L0'];
+                this.modData('Learnsets', 'machamp').learnset.calmmind = ['5M'];
+                this.modData('Learnsets', 'machamp').learnset.calmmind = ['5M'];
+                this.modData('Learnsets', 'machamp').learnset.calmmind = ['5M'];
+                this.modData('Learnsets', 'golem').learnset.rapidspin = ['5L0'];
+                this.modData('Learnsets', 'farfetchd').learnset.closecombat = ['5L52'];
+                this.modData('Learnsets', 'dodrio').learnset.closecombat = ['5L49'];
+                this.modData('Learnsets', 'dewgong').learnset.recover = ['5L37'];
+                this.modData('Learnsets', 'muk').learnset.bulkup = ['5M'];
+                this.modData('Learnsets', 'cloyster').learnset.stealthrock = ['5T'];
+                this.modData('Learnsets', 'gengar').learnset.darkvoid = ['5L85'];
+                this.modData('Learnsets', 'hypno').learnset.darkvoid = ['5L0'];
+                this.modData('Learnsets', 'kingler').learnset.waterfall = ['5M'];
+                this.modData('Learnsets', 'exeggutor').learnset.lowsweep = ['5M'];
+                this.modData('Learnsets', 'exeggutor').learnset.aurasphere = ['5L55'];
+                this.modData('Learnsets', 'marowak').learnset.headsmash = ['5L57'];
+                this.modData('Learnsets', 'hitmonchan').learnset.painsplit = ['5T'];
+                this.modData('Learnsets', 'hitmonlee').learnset.painsplit = ['5T'];
+                this.modData('Learnsets', 'hitmonlee').learnset.painsplit = ['5T'];
+                this.modData('Learnsets', 'weezing').learnset.icebeam = ['5M'];
+                this.modData('Learnsets', 'weezing').learnset.toxicspikes = ['5L42'];
+                this.modData('Learnsets', 'seaking').learnset.hornleech = ['5L0'];
+                this.modData('Learnsets', 'seaking').learnset.swordsdance = ['5M'];
+                this.modData('Learnsets', 'pinsir').learnset.quickattack = ['5L25'];
+                this.modData('Learnsets', 'pinsir').learnset.nightslash = ['5L32'];
+                this.modData('Learnsets', 'pinsir').learnset.crunch = ['5L39'];
+                this.modData('Learnsets', 'pinsir').learnset.pursuit = ['5L0'];
+				this.modData('Learnsets', 'tauros').learnset.headsmash = ['5L55'];
+				this.modData('Learnsets', 'lapras').learnset.dualchop = ['5L55'];
+				this.modData('Learnsets', 'vaporeon').learnset.dragontail = ['5M'];
+				this.modData('Learnsets', 'vaporeon').learnset.glare = ['5L28'];
+				this.modData('Learnsets', 'flareon').learnset.flareblitz = ['5L47'];
+				this.modData('Learnsets', 'flareon').learnset.swordsdance = ['5M'];
+				this.modData('Learnsets', 'flareon').learnset.wildcharge = ['5M'];
+				this.modData('Learnsets', 'aerodactyl').learnset.headsmash = ['5L58'];
                 //Need to finish this lul
             }
 };
