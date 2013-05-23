8e3606dc3b1f8c4d774adaeb67f0299ae9c2a74a
Smog S. Goon <iSmogoon2@gmail.com>
2013-05-22 23:10:24 -0500

    "Finish" Formats

--- mods/projectxy/formats-data.js
+++ mods/projectxy/formats-data.js
@@ -7,4 +7,32 @@ exports.BattleFormatsData = {
 		inherit: true,
 		tier: "Uber"
 	},
+	blaziken: {
+		inherit: true,
+		tier: "OU"
+	},
+	darkrai: {
+		inherit: true,
+		tier: "OU"
+	},
+	shayminsky: {
+		inherit: true,
+		tier: "OU"
+	},
+	genesect: {
+		inherit: true,
+		tier: "OU"
+	},
+	thundurus: {
+		inherit: true,
+		tier: "OU"
+	},
+	tornadustherian: {
+		inherit: true,
+		tier: "OU"
+	},
+	deoxys: {
+		inherit: true,
+		tier: "OU"
+	},
 };
